59065b8818c35401bfe761d19c402b19cf4277f5
abu-al3ees <malkhazali83@gmail.com>
2021-01-29 21:49:48 +0200

    render image

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var number = prompt('enter yourfirst number');
 var number_2 = prompt('enter your second number');
 var sign =prompt('enter your sign only + / * -');
 
+
 while(sign!=='+' && sign!=='*' && sign!=='/'&& sign!=='-'){
   sign = prompt('plz your sign must be  only +  *  / -');
 }
@@ -34,3 +35,10 @@ function multi(n,n1){
     var multi=number * number_2;
     document.write('<h1>' +"your multi=" +multi + '</h1>');
 }
+var userInputs = prompt('how many operation ? choose number');
+var image='<img src="calcu.jpeg">'; 
+for(var i = 0; i < userInputs; i++){
+     
+        console.log(image);
+       document.write(image);
+    }
